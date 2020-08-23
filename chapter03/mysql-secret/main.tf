ab3e89971766c7bd3c5090572a9c1d336824e604
Attila Domokos <adomokos@gmail.com>
2020-08-23 12:07:30 -0500

    Initialize a mysql DB server with terraform

---
+++ chapter03/mysql-secret/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_db_instance" "example" {
+  identifier_prefix = "terraform-up-and-running"
+  engine = "mysql"
+  allocated_storage = 10
+  instance_class = "db.t2.micro"
+  username = "admin"
+  name = var.db_name
+  skip_final_snapshot = true
+  password = var.db_password
+}
